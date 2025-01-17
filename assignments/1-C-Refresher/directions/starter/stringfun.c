6818d95dcf63da4a648d761b9f4af92c6abdb984
Jason Morais <jasonmorais02@gmail.com>
2025-01-17 13:58:34 -0500

    Finished assignement 1 c refresher work

--- assignments/1-C-Refresher/directions/starter/stringfun.c
+++ assignments/1-C-Refresher/directions/starter/stringfun.c
@@ -12,16 +12,39 @@ int  setup_buff(char *, char *, int);
 
 //prototypes for functions to handle required functionality
 int  count_words(char *, int, int);
-//add additional prototypes here
+void* reverseString(char* originalString, char* reversedString, int length);
+void wordPrint(char*);
 
 
 int setup_buff(char *buff, char *user_str, int len){
     //TODO: #4:  Implement the setup buff as per the directions
-    return 0; //for now just so the code compiles. 
+    if (strlen(user_str) > 50){
+        return -1;
+    }
+
+    char lastString = '\0';
+    int outOfBounds = 0;
+    for (int i = 0; i <= len; i++) {
+        if(user_str[i] == ' ' && lastString != ' '){
+            buff[i] = user_str[i];
+        }
+        else if(user_str[i] == '\0'){
+            outOfBounds = 1;
+            buff[i] = '.';
+        }
+        else if(outOfBounds == 1){
+            buff[i] = '.';
+        }
+        else{
+            buff[i] = user_str[i];
+            lastString = user_str[i];
+        }
+    }
+    return strlen(user_str); 
 }
 
 void print_buff(char *buff, int len){
-    printf("Buffer:  ");
+    printf("Buffer: ");
     for (int i=0; i<len; i++){
         putchar(*(buff+i));
     }
@@ -34,8 +57,46 @@ void usage(char *exename){
 }
 
 int count_words(char *buff, int len, int str_len){
-    //YOU MUST IMPLEMENT
-    return 0;
+    int count = 0;
+    for(int i = 0; buff[i] != '\0'; i++){
+        if(buff[i] == ' '){
+            count += 1;
+        }
+        if(buff[i+1] == '.'){
+            count += 1;
+            break;
+        }
+    }
+    return count;
+}
+
+void* reverseString(char* originalString, char* reversedString, int length){
+    for (int i = 0; originalString[i] != '\0'; i++) {
+        reversedString[i] = originalString[length - 1 - i];
+    }
+    reversedString[length] = '\0';
+}
+
+void wordPrint(char* inputString){
+    int charCount = 0;
+    int currentNumber = 1;
+    printf("Word Print\n----------\n1. ");
+    for(int i = 0; inputString[i] != '.'; i++){
+        if(inputString[i] != ' '){
+            printf("%c", inputString[i]);
+        }
+        if(inputString[i] == ' '){
+            currentNumber += 1;
+            printf(" (%i)\n", charCount);
+            printf("%i. ", currentNumber);
+            charCount = 0;
+        }
+        else if(inputString[i+1] == '.'){
+            printf(" (%i)\n", charCount);
+            break;
+        }
+        charCount += 1;
+    }
 }
 
 //ADD OTHER HELPER FUNCTIONS HERE FOR OTHER REQUIRED PROGRAM OPTIONS
@@ -47,9 +108,11 @@ int main(int argc, char *argv[]){
     char opt;               //used to capture user option from cmd line
     int  rc;                //used for return codes
     int  user_str_len;      //length of user supplied string
+    char* reversedString;
 
     //TODO:  #1. WHY IS THIS SAFE, aka what if arv[1] does not exist?
-    //      PLACE A COMMENT BLOCK HERE EXPLAINING
+    //      It is safe because if arv[1] doesnt exist, it will just be null, since when we allocate
+    // storage for an array in c it takes the space but sets the current unused values to null
     if ((argc < 2) || (*argv[1] != '-')){
         usage(argv[0]);
         exit(1);
@@ -66,7 +129,9 @@ int main(int argc, char *argv[]){
     //WE NOW WILL HANDLE THE REQUIRED OPERATIONS
 
     //TODO:  #2 Document the purpose of the if statement below
-    //      PLACE A COMMENT BLOCK HERE EXPLAINING
+    //    The purpose is to check if the number of arguments is more than the amount we can handle, ie
+    // if the number of arguments is more than 3, we call the usage function which tells the user the correct
+    // format of the input, and exits the program.
     if (argc < 3){
         usage(argv[0]);
         exit(1);
@@ -77,8 +142,10 @@ int main(int argc, char *argv[]){
     //TODO:  #3 Allocate space for the buffer using malloc and
     //          handle error if malloc fails by exiting with a 
     //          return code of 99
-    // CODE GOES HERE FOR #3
-
+    buff = malloc(BUFFER_SZ);
+    if (buff == NULL){
+        exit(99);
+    }
 
     user_str_len = setup_buff(buff, input_string, BUFFER_SZ);     //see todos
     if (user_str_len < 0){
@@ -98,6 +165,14 @@ int main(int argc, char *argv[]){
 
         //TODO:  #5 Implement the other cases for 'r' and 'w' by extending
         //       the case statement options
+        case 'r':
+            reversedString = malloc(BUFFER_SZ);
+            reverseString(buff, reversedString, user_str_len);
+            printf("Reversed String: %s\n", reversedString);
+            break;
+        case 'w':
+            wordPrint(buff);
+            break;
         default:
             usage(argv[0]);
             exit(1);
@@ -105,6 +180,7 @@ int main(int argc, char *argv[]){
 
     //TODO:  #6 Dont forget to free your buffer before exiting
     print_buff(buff,BUFFER_SZ);
+    free(buff);
     exit(0);
 }
 
@@ -114,4 +190,9 @@ int main(int argc, char *argv[]){
 //          is a good practice, after all we know from main() that 
 //          the buff variable will have exactly 50 bytes?
 //  
-//          PLACE YOUR ANSWER HERE
+//          Generally speaking it is good to remove the need to perform the length check operation for
+//          any kind of more complex data structure like a string / char array in the case that you need
+//          to allocate another equal sized array for copying or just checking length for any other procedures
+//          Besides this, it also makes it so that in the case that we are using something besides the 50 byte
+//          buffer size, so in the case there are multiple buffer sizes for whatever purpose, the function is
+//          as dumb as possible. This is a good practice for functions as well, to keep thek as dumb as possible.
