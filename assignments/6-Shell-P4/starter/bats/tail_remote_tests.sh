a8b663d5e1a6175ccf708004a74433c5cb09f863
Jason Morais <jasonmorais02@gmail.com>
2025-03-13 21:06:24 -0400

    removed debugg code

--- assignments/6-Shell-P4/starter/bats/tail_remote_tests.sh
+++ assignments/6-Shell-P4/starter/bats/tail_remote_tests.sh
@@ -15,10 +15,6 @@ EOF
 
     stripped_output=$(echo "$output" | tr -d '[:space:]')
 
-    # Get the list of files in the current directory
-    files=$(ls | tr -d '\n')  # Remove newlines for comparison
-
-    # Construct the expected output
     expected_output="socketclientmode:addr:127.0.0.1:1234dsh4>SuccessfulExecution${files}dsh4>InputError-Exitingcmdloopretur rned0"
 
     echo "Stripped Output: $stripped_output"
@@ -32,11 +28,6 @@ echo "hey" | grep h
 EOF
 
     stripped_output=$(echo "$output" | tr -d '[:space:]')
-
-    # Get the list of files in the current directory
-    files=$(ls | tr -d '\n')  # Remove newlines for comparison
-
-    # Construct the expected output
     expected_output="socketclientmode:addr:127.0.0.1:1234dsh4>SuccessfulExecution${files}dsh4>InputError-Exitingcmdloopretur rned0"
 
     echo "Stripped Output: $stripped_output"
@@ -44,18 +35,13 @@ EOF
     [ "$stripped_output" = "$expected_output" ]
 }
 
-@test "Remote Shell - Test Should fail but showcase working cd .. then after running ls {
+@test "Remote Shell - Test Should fail but showcase working cd .. then after running ls" {
     run ./dsh -c <<EOF
 cd ..
 ls
 EOF
 
     stripped_output=$(echo "$output" | tr -d '[:space:]')
-
-    # Get the list of files in the current directory
-    files=$(ls | tr -d '\n')  # Remove newlines for comparison
-
-    # Construct the expected output
     expected_output="socketclientmode:addr:127.0.0.1:1234dsh4>SuccessfulExecution${files}dsh4>InputError-Exitingcmdloopretur rned0"
 
     echo "Stripped Output: $stripped_output"
